f5118a22aec4e800ef2087806680a3326752217e
mfeti <mfeti2175@gmail.com>
2024-11-01 09:14:26 +0300

    fix about page in final touch

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -18,7 +18,7 @@ function About({ handlePopup }) {
         <div
           data-aos="fade"
           data-aos-delay="300"
-          className="bg-white/70 mt-10 px-8 py-10"
+          className="bg-white/70 mt-10 px-8 py-10 max-w-4xl mx-auto"
         >
           <p className="text-dark text-lg font-semibold">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga
@@ -45,8 +45,8 @@ function About({ handlePopup }) {
         </div>
       </div>
       {/* Wave image section */}
-      <div className="absolute top-0 w-full right-0">
-        <img src={vectorWave} alt="vectorWave" className="mx-auto" />
+      <div className="absolute left-0 -top-2 right-0 w-screen">
+        <img src={vectorWave} alt="vectorWave" className="w-full" />
       </div>
     </section>
   );
